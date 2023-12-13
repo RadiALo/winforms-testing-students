8a0d487271c0908678114e0ee362f516ad29f7ff
RadiALo <78644882+RadiALo@users.noreply.github.com>
2023-12-13 05:08:00 +0200

    SQL Connector

---
+++ Classes/Connector.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Odbc;
+using System.Data;
+
+namespace Testing_students
+{
+    public static class Connector
+    {
+        static string ConnectionString = @"Dsn=Testing Students Server;uid=root;pwd=12345;server=localhost;database=testing_students;port=3306";
+
+        public static DataTable ExecuteSelectQuery(string Query)
+        {
+            OdbcConnection conn = new OdbcConnection(ConnectionString);
+            conn.Open();
+            OdbcDataAdapter oda = new OdbcDataAdapter(Query, conn);
+            DataTable dt = new DataTable();
+            oda.Fill(dt);
+            conn.Close();
+            return dt;
+        }
+
+        public static void ExecuteQuery(string Query)
+        {
+            OdbcConnection conn = new OdbcConnection(ConnectionString);
+            conn.Open();
+            OdbcCommand oda = new OdbcCommand(Query, conn);
+            oda.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
