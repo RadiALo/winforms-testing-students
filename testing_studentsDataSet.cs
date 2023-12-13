ebcf7f416ce7d6d36440f32fffb90065fecd9504
RadiALo <78644882+RadiALo@users.noreply.github.com>
2023-12-13 05:07:08 +0200

    Initial Commit

---
+++ testing_studentsDataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace Testing_students
+{
+
+
+    partial class testing_studentsDataSet
+    {
+    }
+}
+
+namespace Testing_students.testing_studentsDataSetTableAdapters {
+    
+    
+    public partial class studentsTableAdapter {
+    }
+}
